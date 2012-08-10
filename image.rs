dcceec8f278093390659e4f74e0a4d6e1f10fc37
Brian Anderson <banderson@mozilla.com>
2012-08-10 15:06:53 -0700

    Add load_from_memory function

--- image.rs
+++ image.rs
@@ -3,6 +3,7 @@ import stb_image::bindgen::*;
 import libc::types::os::arch::c95::c_int;
 import ptr::{addr_of, is_null};
 import str::as_c_str;
+import vec::as_buf;
 import vec::unsafe::from_buf;
 
 class image {
@@ -36,3 +37,19 @@ fn load(path: ~str) -> image unsafe {
     return image(width as uint, height as uint, depth as uint, data);
 }
 
+fn load_from_memory(buffer: &[u8]) -> image unsafe {
+    let mut width = 0 as c_int;
+    let mut height = 0 as c_int;
+    let mut depth = 0 as c_int;
+    let buffer = as_buf(buffer, |bytes, len| {
+        stbi_load_from_memory(bytes, len as c_int, addr_of(width), addr_of(height), addr_of(depth), 0 as c_int)
+    });
+    if is_null(buffer) {
+        fail ~"failed to load image";
+    }
+
+    // FIXME: Shouldn't copy; instead we should use a sendable resource. They
+    // aren't particularly safe yet though.
+    let data = from_buf(buffer, (width * height * depth) as uint);
+    return image(width as uint, height as uint, depth as uint, data);
+}
