60b6a4ec1d2bd63b24383fe31865552d7eedada4
Patrick Walton <pcwalton@mimiga.net>
2012-09-13 16:22:22 -0700

    Fixes for language changes

--- image.rs
+++ image.rs
@@ -3,8 +3,8 @@ use stb_image::bindgen::*;
 use libc::types::os::arch::c95::c_int;
 use ptr::{addr_of, is_null};
 use str::as_c_str;
-use vec::as_buf;
-use vec::unsafe::from_buf;
+use vec::as_imm_buf;
+use vec::raw::from_buf;
 
 struct Image {
     width: uint,
@@ -50,7 +50,7 @@ fn load_from_memory(buffer: &[u8]) -> Option<Image> unsafe {
         let mut height = 0 as c_int;
         let mut depth = 0 as c_int;
         let force_depth = 4 as c_int;
-        let buffer = as_buf(buffer, |bytes, len| {
+        let buffer = as_imm_buf(buffer, |bytes, len| {
             stbi_load_from_memory(bytes, len as c_int, addr_of(width), addr_of(height), addr_of(depth), force_depth)
         });
 
