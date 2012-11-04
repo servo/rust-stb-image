6dc0f1be87fd33fd9b1d8e51b37f1c6086e4988d
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2012-11-04 15:37:29 +1000

    Enable bit depth specification

--- image.rs
+++ image.rs
@@ -23,14 +23,18 @@ pub fn new_image(width: uint, height: uint, depth: uint, data: ~[u8]) -> Image {
 }
 
 pub fn load(path: ~str) -> Option<Image> unsafe {
+    let force_depth = 4;
+    load_with_depth(move path, force_depth)
+}
+
+pub fn load_with_depth(path: ~str, force_depth: uint) -> Option<Image> unsafe {
     do task::unkillable {
         let mut width = 0 as c_int;
         let mut height = 0 as c_int;
         let mut depth = 0 as c_int;
-        let force_depth = 4 as c_int;
         let buffer = as_c_str(path, |bytes| {
-            stbi_load(bytes, to_unsafe_ptr(&width), to_unsafe_ptr(&height), to_unsafe_ptr(&depth),
-                      force_depth)
+            stbi_load(bytes, to_unsafe_ptr(&width), to_unsafe_ptr(&height),
+                      to_unsafe_ptr(&depth), force_depth as c_int)
         });
 
         if is_null(buffer) {
@@ -38,22 +42,27 @@ pub fn load(path: ~str) -> Option<Image> unsafe {
         } else {
             // FIXME: Shouldn't copy; instead we should use a sendable resource. They
             // aren't particularly safe yet though.
-            let data = from_buf_raw(buffer, (width * height * force_depth) as uint);
+            let data = from_buf_raw(buffer, ((width * height) as uint) * force_depth);
             libc::free(buffer as *c_void);
-            Some(new_image(width as uint, height as uint, force_depth as uint, move data))
+            Some(new_image(width as uint, height as uint, force_depth, move data))
         }
     }
 }
 
 pub fn load_from_memory(buffer: &[u8]) -> Option<Image> unsafe {
+        let force_depth = 4;
+        load_from_memory_with_depth(buffer, force_depth)
+}
+
+pub fn load_from_memory_with_depth(buffer: &[u8], force_depth: uint) -> Option<Image> unsafe {
     do task::unkillable {
         let mut width = 0 as c_int;
         let mut height = 0 as c_int;
         let mut depth = 0 as c_int;
-        let force_depth = 4 as c_int;
         let buffer = as_imm_buf(buffer, |bytes, len| {
             stbi_load_from_memory(bytes, len as c_int, to_unsafe_ptr(&width),
-                                  to_unsafe_ptr(&height), to_unsafe_ptr(&depth), force_depth)
+                                  to_unsafe_ptr(&height), to_unsafe_ptr(&depth),
+                                  force_depth as c_int)
         });
 
         if is_null(buffer) {
@@ -61,9 +70,9 @@ pub fn load_from_memory(buffer: &[u8]) -> Option<Image> unsafe {
         } else {
             // FIXME: Shouldn't copy; instead we should use a sendable resource. They
             // aren't particularly safe yet though.
-            let data = from_buf_raw(buffer, (width * height * force_depth) as uint);
+            let data = from_buf_raw(buffer, ((width * height) as uint) * force_depth);
             libc::free(buffer as *c_void);
-            Some(new_image(width as uint, height as uint, force_depth as uint, move data))
+            Some(new_image(width as uint, height as uint, force_depth, move data))
         }
     }
 }
