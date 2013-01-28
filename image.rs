5e4b7bb128735ac9d169c41fb49c5ef53841fcc3
Josh Matthews <josh@joshmatthews.net>
2013-01-28 13:34:12 +0000

    Rust upgrade.

--- image.rs
+++ image.rs
@@ -28,75 +28,80 @@ enum LoadResult {
     ImageF32(Image<f32>),
 }
 
-pub fn load(path: ~str) -> LoadResult unsafe {
+pub fn load(path: ~str) -> LoadResult {
     let force_depth = 0;
     load_with_depth(move path, force_depth, false)
 }
 
 
-priv fn load_internal<T>(buf : *T, w : c_int, h : c_int, d : c_int) -> Image<T> unsafe   {
-    // FIXME: Shouldn't copy; instead we should use a sendable resource. They
-    // aren't particularly safe yet though.
-    let data = from_buf_raw(buf, (w * h * d) as uint);
-    libc::free(buf as *c_void);
-    Image::<T>{
-        width   : w as uint,
-        height  : h as uint,
-        depth   : d as uint,
-        data    : data}
-}
-
-pub fn load_with_depth(path: ~str, force_depth: uint, convert_hdr:bool) -> LoadResult unsafe {
-    do task::unkillable {
-        let mut width   = 0 as c_int;
-        let mut height  = 0 as c_int;
-        let mut depth   = 0 as c_int;
-        as_c_str(path, |bytes| {
-            if !convert_hdr && stbi_is_hdr(bytes)!=0   {
-                let buffer = stbi_loadf(
-                    bytes, to_unsafe_ptr(&width), to_unsafe_ptr(&height),
-                    to_unsafe_ptr(&depth), force_depth as c_int);
-                if is_null(buffer) {
-                    Error
-                } else {
-                    ImageF32( load_internal(buffer,width,height,depth) )
-                }
-            }else   {
-                let buffer = stbi_load(
-                    bytes, to_unsafe_ptr(&width), to_unsafe_ptr(&height),
-                    to_unsafe_ptr(&depth), force_depth as c_int);
-                if is_null(buffer) {
-                    Error
-                } else {
-                    ImageU8( load_internal(buffer,width,height,depth) )
-                }
-            }
-        })
+priv fn load_internal<T>(buf : *T, w : c_int, h : c_int, d : c_int) -> Image<T> {
+    unsafe {
+        // FIXME: Shouldn't copy; instead we should use a sendable resource. They
+        // aren't particularly safe yet though.
+        let data = from_buf_raw(buf, (w * h * d) as uint);
+        libc::free(buf as *c_void);
+        Image::<T>{
+            width   : w as uint,
+            height  : h as uint,
+            depth   : d as uint,
+            data    : data}
     }
 }
 
-pub fn load_from_memory(buffer: &[u8]) -> LoadResult unsafe {
-        let force_depth = 0;
-        load_from_memory_with_depth(buffer, force_depth, false)
+pub fn load_with_depth(path: ~str, force_depth: uint, convert_hdr:bool) -> LoadResult {
+    unsafe {
+        do task::unkillable {
+            let mut width   = 0 as c_int;
+            let mut height  = 0 as c_int;
+            let mut depth   = 0 as c_int;
+            as_c_str(path, |bytes| {
+                if !convert_hdr && stbi_is_hdr(bytes)!=0   {
+                    let buffer = stbi_loadf(
+                        bytes, to_unsafe_ptr(&width), to_unsafe_ptr(&height),
+                        to_unsafe_ptr(&depth), force_depth as c_int);
+                    if is_null(buffer) {
+                        Error
+                    } else {
+                        ImageF32( load_internal(buffer,width,height,depth) )
+                    }
+                } else {
+                    let buffer = stbi_load(
+                        bytes, to_unsafe_ptr(&width), to_unsafe_ptr(&height),
+                        to_unsafe_ptr(&depth), force_depth as c_int);
+                    if is_null(buffer) {
+                        Error
+                    } else {
+                        ImageU8( load_internal(buffer,width,height,depth) )
+                    }
+                }
+            })
+        }
+    }
 }
 
-pub fn load_from_memory_with_depth(buffer: &[u8], force_depth: uint, convert_hdr:bool) -> LoadResult unsafe {
-    do task::unkillable {
-        let mut width = 0 as c_int;
-        let mut height = 0 as c_int;
-        let mut depth = 0 as c_int;
-        as_imm_buf(buffer, |bytes, len| {
-            if !convert_hdr && stbi_is_hdr_from_memory(bytes,len as c_int)!=0   {
-                let buffer = stbi_loadf_from_memory(
-                    bytes, len as c_int, to_unsafe_ptr(&width),
-                    to_unsafe_ptr(&height), to_unsafe_ptr(&depth),
-                    force_depth as c_int);
-                if is_null(buffer) {
-                    Error
-                } else {
-                    ImageF32( load_internal(buffer,width,height,depth) )
-                }
-            }else   {
+pub fn load_from_memory(buffer: &[u8]) -> LoadResult {
+    let force_depth = 0;
+    load_from_memory_with_depth(buffer, force_depth, false)
+}
+
+pub fn load_from_memory_with_depth(buffer: &[u8], force_depth: uint, convert_hdr:bool) -> LoadResult {
+    unsafe {
+        do task::unkillable {
+            let mut width = 0 as c_int;
+            let mut height = 0 as c_int;
+            let mut depth = 0 as c_int;
+            as_imm_buf(buffer, |bytes, len| {
+                if !convert_hdr && stbi_is_hdr_from_memory(bytes,len as c_int)!=0   {
+                    let buffer = stbi_loadf_from_memory(
+                        bytes, len as c_int, to_unsafe_ptr(&width),
+                        to_unsafe_ptr(&height), to_unsafe_ptr(&depth),
+                        force_depth as c_int);
+                    if is_null(buffer) {
+                        Error
+                    } else {
+                        ImageF32( load_internal(buffer,width,height,depth) )
+                    }
+                }else {
                 let buffer = stbi_load_from_memory(
                     bytes, len as c_int, to_unsafe_ptr(&width),
                     to_unsafe_ptr(&height), to_unsafe_ptr(&depth),
