d842173a9cc73fd5c958cf19ed1f159ff4eb99d5
Sébastien Crozet <developer@crozet.re>
2013-07-21 10:17:42 +0200

    Update to new rust syntax.
    `pub extern' is now obsolete. The `pub' modifier must be specified for each individual items.

--- stb_image.rs
+++ stb_image.rs
@@ -35,65 +35,65 @@ pub mod bindgen {
     eof: *u8,
   }
 
-  pub extern {
+extern {
 
-fn stbi_load_from_memory(buffer: *stbi_uc, len: c_int, x: *mut c_int, y: *mut c_int, comp: *mut c_int, req_comp: c_int) -> *stbi_uc;
+pub fn stbi_load_from_memory(buffer: *stbi_uc, len: c_int, x: *mut c_int, y: *mut c_int, comp: *mut c_int, req_comp: c_int) -> *stbi_uc;
 
-fn stbi_load(filename: *c_char, x: *mut c_int, y: *mut c_int, comp: *mut c_int, req_comp: c_int) -> *stbi_uc;
+pub fn stbi_load(filename: *c_char, x: *mut c_int, y: *mut c_int, comp: *mut c_int, req_comp: c_int) -> *stbi_uc;
 
-fn stbi_load_from_file(f: *FILE, x: *mut c_int, y: *mut c_int, comp: *mut c_int, req_comp: c_int) -> *stbi_uc;
+pub fn stbi_load_from_file(f: *FILE, x: *mut c_int, y: *mut c_int, comp: *mut c_int, req_comp: c_int) -> *stbi_uc;
 
-fn stbi_load_from_callbacks(clbk: *stbi_io_callbacks, user: *c_void, x: *mut c_int, y: *mut c_int, comp: *mut c_int, req_comp: c_int) -> *stbi_uc;
+pub fn stbi_load_from_callbacks(clbk: *stbi_io_callbacks, user: *c_void, x: *mut c_int, y: *mut c_int, comp: *mut c_int, req_comp: c_int) -> *stbi_uc;
 
-fn stbi_loadf_from_memory(buffer: *stbi_uc, len: c_int, x: *mut c_int, y: *mut c_int, comp: *mut c_int, req_comp: c_int) -> *c_float;
+pub fn stbi_loadf_from_memory(buffer: *stbi_uc, len: c_int, x: *mut c_int, y: *mut c_int, comp: *mut c_int, req_comp: c_int) -> *c_float;
 
-fn stbi_loadf(filename: *c_char, x: *mut c_int, y: *mut c_int, comp: *mut c_int, req_comp: c_int) -> *c_float;
+pub fn stbi_loadf(filename: *c_char, x: *mut c_int, y: *mut c_int, comp: *mut c_int, req_comp: c_int) -> *c_float;
 
-fn stbi_loadf_from_file(f: *FILE, x: *mut c_int, y: *mut c_int, comp: *mut c_int, req_comp: c_int) -> *c_float;
+pub fn stbi_loadf_from_file(f: *FILE, x: *mut c_int, y: *mut c_int, comp: *mut c_int, req_comp: c_int) -> *c_float;
 
-fn stbi_loadf_from_callbacks(clbk: *stbi_io_callbacks, user: *c_void, x: *mut c_int, y: *mut c_int, comp: *mut c_int, req_comp: c_int) -> *c_float;
+pub fn stbi_loadf_from_callbacks(clbk: *stbi_io_callbacks, user: *c_void, x: *mut c_int, y: *mut c_int, comp: *mut c_int, req_comp: c_int) -> *c_float;
 
-fn stbi_hdr_to_ldr_gamma(gamma: c_float);
+pub fn stbi_hdr_to_ldr_gamma(gamma: c_float);
 
-fn stbi_hdr_to_ldr_scale(scale: c_float);
+pub fn stbi_hdr_to_ldr_scale(scale: c_float);
 
-fn stbi_ldr_to_hdr_gamma(gamma: c_float);
+pub fn stbi_ldr_to_hdr_gamma(gamma: c_float);
 
-fn stbi_ldr_to_hdr_scale(scale: c_float);
+pub fn stbi_ldr_to_hdr_scale(scale: c_float);
 
-fn stbi_is_hdr_from_callbacks(clbk: *stbi_io_callbacks, user: *c_void) -> c_int;
+pub fn stbi_is_hdr_from_callbacks(clbk: *stbi_io_callbacks, user: *c_void) -> c_int;
 
-fn stbi_is_hdr_from_memory(buffer: *stbi_uc, len: c_int) -> c_int;
+pub fn stbi_is_hdr_from_memory(buffer: *stbi_uc, len: c_int) -> c_int;
 
-fn stbi_is_hdr(filename: *c_char) -> c_int;
+pub fn stbi_is_hdr(filename: *c_char) -> c_int;
 
-fn stbi_is_hdr_from_file(f: *FILE) -> c_int;
+pub fn stbi_is_hdr_from_file(f: *FILE) -> c_int;
 
-fn stbi_failure_reason() -> *c_char;
+pub fn stbi_failure_reason() -> *c_char;
 
-fn stbi_image_free(retval_from_stbi_load: *c_void);
+pub fn stbi_image_free(retval_from_stbi_load: *c_void);
 
-fn stbi_info_from_memory(buffer: *stbi_uc, len: c_int, x: *c_int, y: *c_int, comp: *c_int) -> c_int;
+pub fn stbi_info_from_memory(buffer: *stbi_uc, len: c_int, x: *c_int, y: *c_int, comp: *c_int) -> c_int;
 
-fn stbi_info_from_callbacks(clbk: *stbi_io_callbacks, user: *c_void, x: *c_int, y: *c_int, comp: *c_int) -> c_int;
+pub fn stbi_info_from_callbacks(clbk: *stbi_io_callbacks, user: *c_void, x: *c_int, y: *c_int, comp: *c_int) -> c_int;
 
-fn stbi_info(filename: *c_char, x: *c_int, y: *c_int, comp: *c_int) -> c_int;
+pub fn stbi_info(filename: *c_char, x: *c_int, y: *c_int, comp: *c_int) -> c_int;
 
-fn stbi_info_from_file(f: *FILE, x: *c_int, y: *c_int, comp: *c_int) -> c_int;
+pub fn stbi_info_from_file(f: *FILE, x: *c_int, y: *c_int, comp: *c_int) -> c_int;
 
-fn stbi_set_unpremultiply_on_load(flag_true_if_should_unpremultiply: c_int);
+pub fn stbi_set_unpremultiply_on_load(flag_true_if_should_unpremultiply: c_int);
 
-fn stbi_convert_iphone_png_to_rgb(flag_true_if_should_convert: c_int);
+pub fn stbi_convert_iphone_png_to_rgb(flag_true_if_should_convert: c_int);
 
-fn stbi_zlib_decode_malloc_guesssize(buffer: *c_char, len: c_int, initial_size: c_int, outlen: *c_int) -> *c_char;
+pub fn stbi_zlib_decode_malloc_guesssize(buffer: *c_char, len: c_int, initial_size: c_int, outlen: *c_int) -> *c_char;
 
-fn stbi_zlib_decode_malloc(buffer: *c_char, len: c_int, outlen: *c_int) -> *c_char;
+pub fn stbi_zlib_decode_malloc(buffer: *c_char, len: c_int, outlen: *c_int) -> *c_char;
 
-fn stbi_zlib_decode_buffer(obuffer: *c_char, olen: c_int, ibuffer: *c_char, ilen: c_int) -> c_int;
+pub fn stbi_zlib_decode_buffer(obuffer: *c_char, olen: c_int, ibuffer: *c_char, ilen: c_int) -> c_int;
 
-fn stbi_zlib_decode_noheader_malloc(buffer: *c_char, len: c_int, outlen: *c_int) -> *c_char;
+pub fn stbi_zlib_decode_noheader_malloc(buffer: *c_char, len: c_int, outlen: *c_int) -> *c_char;
 
-fn stbi_zlib_decode_noheader_buffer(obuffer: *c_char, olen: c_int, ibuffer: *c_char, ilen: c_int) -> c_int;
+pub fn stbi_zlib_decode_noheader_buffer(obuffer: *c_char, olen: c_int, ibuffer: *c_char, ilen: c_int) -> c_int;
 
   }
 }
